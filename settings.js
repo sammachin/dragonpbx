458f8ca211fca9ea6dbad0a8ccacf1a2ed42fcf5
Sam Machin <sam@sammachin.com>
2025-08-28 08:55:25 +0100

    update config & settings

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const DRACHTIO_HOST = process.env.DRACHTIO_HOST || 'sbc.sammachin.com';
 const DRACHTIO_PORT = process.env.DRACHTIO_PORT || 9022;
 const RTPENGINE_HOST = process.env.RTPENGINE_HOST || DRACHTIO_HOST;
-const RTPENGINE_PORT = process.env.RTPENGINE_PORT || 2223
+const RTPENGINE_PORT = Number(process.env.RTPENGINE_PORT) || 2223
 const DRACHTIO_SECRET = process.env.DRACHTIO_SECRET || 'cymru';
 const LOGLEVEL = process.env.LOGLEVEL || 'debug';
 const WEBPORT = process.env.WEBPORT || 2999;
@@ -11,15 +11,15 @@ const DB_PASSWORD = process.env.DB_PASSWORD || 'my_password'
 const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_PORT = process.env.DB_PORT || 5432
 
-const  HTTP_POOL = process.env.HTTP_POOL || false
-const  HTTP_POOLSIZE = process.env.HTTP_POOLSIZE || 3
-const  HTTP_PIPELINING = process.env.HTTP_PIPELINING || false
-const  HTTP_TIMEOUT = process.env.HTTP_TIMEOUT || 30
-const  HTTP_PROXY_IP = process.env.HTTP_PROXY_IP || false
-const  HTTP_PROXY_PORT = process.env.HTTP_PROXY_PORT || false
-const  HTTP_PROXY_PROTOCOL = process.env.HTTP_PROXY_PROTOCOL || false
-const  NODE_ENV = process.env.NODE_ENV 
-const  HTTP_USER_AGENT_HEADER = process.env.HTTP_USER_AGENT_HEADER || 'dragonpbx'
+const HTTP_POOL = process.env.HTTP_POOL || false
+const HTTP_POOLSIZE = process.env.HTTP_POOLSIZE || 3
+const HTTP_PIPELINING = process.env.HTTP_PIPELINING || false
+const HTTP_TIMEOUT = process.env.HTTP_TIMEOUT || 30
+const HTTP_PROXY_IP = process.env.HTTP_PROXY_IP || false
+const HTTP_PROXY_PORT = process.env.HTTP_PROXY_PORT || false
+const HTTP_PROXY_PROTOCOL = process.env.HTTP_PROXY_PROTOCOL || false
+const NODE_ENV = process.env.NODE_ENV 
+const HTTP_USER_AGENT_HEADER = process.env.HTTP_USER_AGENT_HEADER || 'dragonpbx'
 const MEDIA_PATH = process.env.MEDIA_PATH || __dirname
 const FILE_PATH = process.env.FILE_PATH || __dirname
 
