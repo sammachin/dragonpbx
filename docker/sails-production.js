a4549ac185caa6d12d0db7f39ea28c164bc99a19
Sam Machin <sam@sammachin.com>
2026-03-26 08:34:33 +0000

    move docker to separte repo

--- docker/sails-production.js
+++
@@ -1,9 +0,0 @@
-module.exports.sockets = {
-  onlyAllowOrigins: (process.env.ALLOWED_ORIGINS || 'http://localhost:1337').split(',')
-};
-
-module.exports.tokens = {};
-
-if (process.env.MASTER_API_TOKEN) {
-  module.exports.tokens[process.env.MASTER_API_TOKEN] = 1;
-}
