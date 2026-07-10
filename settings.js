288e5128a8b4ea4f766ff0b66d05084030bed5c0
Sam Machin <sam@sammachin.com>
2026-07-10 14:25:45 +0100

    reg trunk fixes
    use shorter reg-id and confugrable expires

--- settings.js
+++ settings.js
@@ -38,6 +38,7 @@ const CONFIG_TTL = process.env.CONFIG_TTL || 60
 const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
 
 const REGTRUNKREFRESH = Number(process.env.REGTRUNKREFRESH) * 1000 || 60000
+const REGTRUNKEXPIRES = Number(process.env.REGTRUNKEXPIRES) || 3600
 
 module.exports = {
   DRACHTIO_HOST,
@@ -72,5 +73,6 @@ module.exports = {
   CONFIG_TTL,
   CONFIG_URL,
   REDIS_URL,
-  REGTRUNKREFRESH
+  REGTRUNKREFRESH,
+  REGTRUNKEXPIRES
 }
